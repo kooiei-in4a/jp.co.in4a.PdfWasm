42938ece41ddad9103b70df7aac2dd973b2f3a4c
kouichirou ie <kouichirou.ie@in4a.jp>
2025-06-14 12:00:50 +0900

    Syncfusion ライセンスの登録と初期化の変更
    `Syncfusion.Licensing` 名前空間を追加し、ライセンスを登録しました。
    アプリケーションのルートコンポーネントを指定し、MudBlazor サービスの登録を削除しました。
    これにより、アプリケーションの初期化プロセスが調整されました。

--- jp.co.in4a.PdfWasm/Program.cs
+++ jp.co.in4a.PdfWasm/Program.cs
@@ -2,14 +2,16 @@ using jp.co.in4a.PdfWasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Syncfusion.Licensing;
+
+SyncfusionLicenseProvider.RegisterLicense("YourKey");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
